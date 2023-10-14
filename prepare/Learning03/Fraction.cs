ab5cf29cf29cea49f4f28c0a0d2e595c874cd5fc
Victória Lourenço <106984193+viclourenco@users.noreply.github.com>
2023-10-14 12:23:48 -0300

    Fraction.cs

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,41 @@
+using System;
+
+class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    // Constructor 1
+    public Fraction()
+    {
+    _top = 1;
+    _bottom = 1; 
+    }
+    // Constructor 2
+    public Fraction(int wholeNumber)
+    {
+    _top = wholeNumber;
+    _bottom = 1; 
+    }
+    // Constructor 3
+    public Fraction(int top, int bottom)
+    {
+    _top = top;
+    _bottom = bottom; 
+    }
+
+    public string GetFractionString()
+    {
+    string text = $"{_top}/{_bottom}";
+    return text;
+    }
+
+    public double GetDecimalValue()
+    {
+    return (double)_top / (double)_bottom;
+    }
+
+
+
+
+}
